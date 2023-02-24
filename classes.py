71992b88563105f1abac0c896d428b7c192c0bae
Isabelleatt <86394099+Isabelleatt@users.noreply.github.com>
2023-02-24 13:51:04 -0300

    Implementando assets do jogo

--- classes.py
+++ classes.py
@@ -3,10 +3,16 @@ from random import *
 from math import *
 
 #VARIAVEIS
-WIDHT = 1000
+WIDTH = 1000
 HEIGHT = 5500
 
 #CORES
 
 class Jogo:
-    
+    # Desenhar circulo
+    def desenha_circulo(self, x, y, raio, cor):
+
+        pygame.draw.circle(self.tela, cor, (x, y), raio)
+        self.tela.blit(self.tela, (0, 0))
+
+

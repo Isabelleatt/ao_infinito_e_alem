8344f24c167905af0edd4b17409ae2857eba148f
liviatanaka <liviatanaka19@gmail.com>
2023-02-25 22:35:05 -0300

    adicionando tela de derrota

--- telas/Tela_derrota.py
+++ telas/Tela_derrota.py
@@ -1,14 +1,14 @@
 import pygame
 import numpy as np
 from multiuso import *
-from telas.Tela_inicial import Tela_inicial
+from telas.Tela_menu import Tela_menu
 from info_fases import info_fases
 
 class Tela_derrota():
     def __init__(self, fase_atual):
         self.fundo = pygame.image.load('Assets\\resultado\perdeu.png').convert_alpha()
-        self.rect_botao_menu = (425,309,230,40)
-        self.rect_botao_again = (425,437,240,60)
+        self.rect_botao_menu = (425,309,280,40)
+        self.rect_botao_again = (425,437,280,60)
         self.fase_atual = fase_atual
         self.nivel = fase_atual.nivel
     
@@ -19,7 +19,7 @@ class Tela_derrota():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     if colisao_ponto_retangulo(self.rect_botao_menu, event.pos[0],event.pos[1]):
-                        return Tela_inicial()
+                        return Tela_menu(self.nivel)
                     elif colisao_ponto_retangulo(self.rect_botao_again, event.pos[0],event.pos[1]):  
                         tela = self.fase_atual
                         return tela
--- telas/Tela_menu.py
+++ telas/Tela_menu.py
@@ -4,12 +4,13 @@ from multiuso import *
 from fases.Fase import *
 from info_fases import info_fases
 
-class Tela_inicial():
-    def __init__(self):
+class Tela_menu():
+    def __init__(self, nivel_atual):
         self.fundo = pygame.image.load('Assets\\tela_inicial\menu_inicial.png').convert_alpha()
         self.rect_botao_play = (360,370,230,40)
         self.rect_botao_regras = (685,450,240,56)
-    
+        self.nivel_atual = nivel_atual    
+
     def atualiza(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,7 +18,8 @@ class Tela_inicial():
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     if colisao_ponto_retangulo(self.rect_botao_play, event.pos[0],event.pos[1]):  
-                        i = 0
+                        i = self.nivel_atual
+
                         tela = Fase(
                             planetas = info_fases[i]['planetas'],
                             pos_inicial = info_fases[i]['pos_inicial'],
